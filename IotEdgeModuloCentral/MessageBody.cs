d61ad4f114a5fdeeb150c33bcb8e025b72438c71
peneluc <peneluc@gmail.com>
2020-03-16 11:16:57 -0300

    Última versão publicada

---
+++ IotEdgeModuloCentral/MessageBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace peneluc_iot_edge_central
+{
+    class MessageBody
+    {
+        public Machine machine { get; set; }
+        public Ambient ambient { get; set; }
+        public string timeCreated { get; set; }
+    }
+}
--- IotEdgeModuloCentral/Program.cs
+++ IotEdgeModuloCentral/Program.cs
@@ -1,25 +1,47 @@
-namespace IotEdgeModuloCentral
+namespace peneluc_iot_edge_central
 {
+    using Microsoft.Azure.Devices.Client;
     using System;
-    using System.IO;
-    using System.Runtime.InteropServices;
     using System.Runtime.Loader;
-    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.Azure.Devices.Client;
-    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using System.Collections.Generic;     // For KeyValuePair<>
+    using Microsoft.Azure.Devices.Shared; // For TwinCollection
+    using Newtonsoft.Json;                // For JsonConvert
 
     class Program
     {
-        static int counter;
+        /// <summary>
+        /// Propriedade que armazena o contador de mensagens *encaminhadas*
+        /// </summary>
+        private static int _totalMensagensEncaminhadas;
+
+        /// <summary>
+        /// Propriedade que armazena o contador de mensagens *recebidas*
+        /// </summary>
+        private static int _totalMensagensRecebidas;
+
+        /// <summary>
+        /// Propriedade que indica a temperatura limite para envio de alerta
+        /// </summary>
+        private static int _temperaturaLimite = 25;
+
+        /// <summary>
+        /// Propriedade com nome do módulo de *entrada* das mensagens
+        /// </summary>
+        private static readonly string _inputName = "input1";
+
+        /// <summary>
+        /// Propriedade com nome do módulo de *saída* das mensagens
+        /// </summary>
+        private static readonly string _outputName = "output1";
 
         static void Main(string[] args)
         {
             Init().Wait();
 
-            // Wait until the app unloads or is cancelled
+            // Aguarda até o aplicativo descarregar ou ser cancelado
             var cts = new CancellationTokenSource();
             AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
             Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
@@ -27,7 +49,7 @@ namespace IotEdgeModuloCentral
         }
 
         /// <summary>
-        /// Handles cleanup operations when app is cancelled or unloads
+        /// Lida com operações de limpeza quando o aplicativo é cancelado ou descarregado
         /// </summary>
         public static Task WhenCancelled(CancellationToken cancellationToken)
         {
@@ -37,53 +59,153 @@ namespace IotEdgeModuloCentral
         }
 
         /// <summary>
-        /// Initializes the ModuleClient and sets up the callback to receive
-        /// messages containing temperature information
+        /// Inicializa o ModuleClient e configura o retorno de chamada para receber
+        /// mensagens contendo informações de temperatura
         /// </summary>
         static async Task Init()
         {
             AmqpTransportSettings amqpSetting = new AmqpTransportSettings(TransportType.Amqp_Tcp_Only);
             ITransportSettings[] settings = { amqpSetting };
 
-            // Open a connection to the Edge runtime
+            // Abra uma conexão com o tempo de execução do Edge
             ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
             await ioTHubModuleClient.OpenAsync();
-            Console.WriteLine("IoT Hub module client initialized.");
+            Console.WriteLine("*** Cliente do módulo IoT Hub inicializado ***");
 
-            // Register callback to be called when a message is received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            // Obtem os valores das *propriedades desejadas* do módulo gêmeo
+            var moduleTwin = await ioTHubModuleClient.GetTwinAsync();
+
+            // Atribui *propriedades desejadas* ao método de tratamento
+            await OnDesiredPropertiesUpdate(moduleTwin.Properties.Desired, ioTHubModuleClient);
+
+            // Anexa método para tratar as *propriedades desejadas* do módulo gêmeo sempre que tiver atualizações.
+            await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertiesUpdate, null);
+
+            // Registra um método responsável por tratar as mensagens recebidas pelo módulo (filtrar e encaminhar).
+            await ioTHubModuleClient.SetInputMessageHandlerAsync(_inputName, FilterMessages, ioTHubModuleClient);
         }
-
-        /// <summary>
-        /// This method is called whenever the module is sent a message from the EdgeHub. 
-        /// It just pipe the messages without any change.
-        /// It prints all the incoming messages.
-        /// </summary>
-        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        
+        /// Este método recebe atualizações das propriedades desejadas do módulo twin e 
+        /// atualiza a variável temperatureThreshold para corresponder.Todos os módulos 
+        /// possuem seu próprio módulo duplo, o que permite configurar o código que está 
+        /// sendo executado dentro de um módulo diretamente da nuvem.
+        static Task OnDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
         {
-            int counterValue = Interlocked.Increment(ref counter);
-
-            var moduleClient = userContext as ModuleClient;
-            if (moduleClient == null)
+            Console.WriteLine($"[OnDesiredPropertiesUpdate]");
+            Console.WriteLine($"[Este método recebe atualizações das propriedades desejadas do módulo twin e atualiza a variável temperatureThreshold para corresponder]");
+            try
             {
-                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+                Console.WriteLine("Desired property change:");
+                Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+
+                if (desiredProperties["TemperatureThreshold"] != null)
+                    _temperaturaLimite = desiredProperties["TemperatureThreshold"];
             }
-
-            byte[] messageBytes = message.GetBytes();
-            string messageString = Encoding.UTF8.GetString(messageBytes);
-            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
-
-            if (!string.IsNullOrEmpty(messageString))
+            catch (AggregateException ex)
             {
-                var pipeMessage = new Message(messageBytes);
-                foreach (var prop in message.Properties)
+                foreach (Exception exception in ex.InnerExceptions)
                 {
-                    pipeMessage.Properties.Add(prop.Key, prop.Value);
+                    Console.WriteLine();
+                    Console.WriteLine("Error when receiving desired property: {0}", exception);
                 }
-                await moduleClient.SendEventAsync("output1", pipeMessage);
-                Console.WriteLine("Received message sent");
             }
-            return MessageResponse.Completed;
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error when receiving desired property: {0}", ex.Message);
+            }
+            return Task.CompletedTask;
         }
+
+        /// Esse método é chamado sempre que o módulo recebe uma mensagem do hub IoT Edge.
+        /// Ele filtra as mensagens que relatam temperaturas abaixo do limite de temperatura 
+        /// definido pelo módulo duplo.Ele também adiciona a propriedade MessageType à mensagem 
+        /// com o valor definido como Alerta.
+        static async Task<MessageResponse> FilterMessages(Message message, object userContext)
+        {
+            Console.WriteLine($"[FilterMessages]");
+            Console.WriteLine($"[Esse método é chamado sempre que o módulo recebe uma mensagem do hub IoT Edge]");
+            try
+            {
+                ModuleClient moduleClient = (ModuleClient)userContext;
+                if (moduleClient == null)
+                {
+                    throw new InvalidOperationException("Módulo cliente não instanciado");
+                }
+
+                var messageBytes = message.GetBytes();
+                if(messageBytes==null)
+                {
+                    throw new InvalidOperationException("messageBytes igual a null!");
+                }
+
+                var messageString = Encoding.UTF8.GetString(messageBytes);
+                var totalMensagensRecebidas = Interlocked.Increment(ref _totalMensagensRecebidas);
+                Console.WriteLine($"Mensagem recebida! Total: {totalMensagensRecebidas}, Corpo da Mensagem: {messageString}");
+
+                // Captura corpo da mensagem recebida
+                MessageBody messageBody = null;
+                try
+                {
+                    messageBody = JsonConvert.DeserializeObject<MessageBody>(messageString);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro na captura corpo da mensagem: {ex.Message}");
+                    if (string.IsNullOrEmpty(messageString))
+                    {
+                        throw new InvalidOperationException($"Falha ao deserializar messageString: {messageString}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"messageString: {messageString}");
+                    }
+                }
+
+                if (messageBody != null && messageBody.machine.temperature > _temperaturaLimite)
+                {
+                    Console.WriteLine($"Temperatura da máquina {messageBody.machine.temperature} excede o limite {_temperaturaLimite}");
+                    using (var filteredMessage = new Message(messageBytes))
+                    {
+                        foreach (KeyValuePair<string, string> prop in message.Properties)
+                        {
+                            filteredMessage.Properties.Add(prop.Key, prop.Value);
+                        }
+
+                        filteredMessage.Properties.Add("MessageType", "Alert");
+                        await moduleClient.SendEventAsync(_outputName, filteredMessage);
+
+                        var totalMensagensEncaminhadas = Interlocked.Increment(ref _totalMensagensEncaminhadas);
+                        Console.WriteLine($"Mensagem recebida encaminhada para o Iot Hub! Total: {totalMensagensEncaminhadas}, Corpo da Mensagem: {_outputName}");
+                    }
+                }
+
+                // Indica que o tratamento da mensagem FOI concluído.
+                return MessageResponse.Completed;
+            }
+            catch (AggregateException ex)
+            {
+                int cont=0;
+                foreach (Exception exception in ex.InnerExceptions)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("FilterMessages :: Erro{0}: {1}", cont++, exception);
+                }
+
+                // Indica que o tratamento da mensagem NÃO foi concluído.
+                var moduleClient = (ModuleClient)userContext;
+                return MessageResponse.Abandoned;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("FilterMessages :: Erro: {0}", ex.Message);
+
+                // Indica que o tratamento da mensagem NÃO foi concluído.
+                ModuleClient moduleClient = (ModuleClient)userContext;
+                return MessageResponse.Abandoned;
+            }
+        }
+
     }
 }
